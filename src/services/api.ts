1e42434ae47be36f1b0bac42db944214307167fa
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-08-05 01:37:22 -0300

    changing database url in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "postgresql://refund_user:mbJgnlTF5Ht6rp79IlYKHuppOxZugGKM@dpg-d28nc8ruibrs73dnkje0-a.oregon-postgres.render.com/refund",
+  baseURL: "https://refund-api-jqj9.onrender.com",
 });
