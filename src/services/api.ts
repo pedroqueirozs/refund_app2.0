60e3b504e8aedd539d393f30d974f1d90deefc18
Pedro Queiroz <105246645+pedroqueirozs@users.noreply.github.com>
2025-08-05 00:38:00 -0300

    update the database url  api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "postgresql://refund_user:mbJgnlTF5Ht6rp79IlYKHuppOxZugGKM@dpg-d28nc8ruibrs73dnkje0-a.oregon-postgres.render.com/refund",
 });
